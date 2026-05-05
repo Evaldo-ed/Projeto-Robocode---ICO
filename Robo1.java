e12d90f0099e495503438a9ae4037b63546159fc
Gabriel Rocha Flores <gabriel.rf2008@aluno.ifsc.edu.br>
2026-05-05 20:23:23 -0300

    Atualizei o Robo1 para ter um movimento similar ao robo-spin

--- Robo1.java
+++ Robo1.java
@@ -3,11 +3,16 @@ import robocode.*;
 //import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
 
 /**
  * Robo1 - a robot by (your name here)
  */
-public class Robo1 extends Robot
+public class Robo1 extends AdvancedRobot
 {
 	/**
 	 * run: Robo1's default behavior
@@ -18,15 +23,13 @@ public class Robo1 extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setColors(Color.blue,Color.blue,Color.red); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			setTurnRight(10000);
+			setMaxVelocity(5);
+			ahead(10000);
 		}
 	}
 
@@ -35,22 +38,31 @@ public class Robo1 extends Robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
-		fire(1);
+		fire(3);
 	}
 
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() > -10 && e.getBearing() < 10) {
+			fire(3);
+		}
+		if (e.isMyFault()) {
+			turnRight(10);
+		}
+	}
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		back(10);
+		back(100);
+		setTurnLeft(100);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
-	public void onHitWall(HitWallEvent e) {
+//	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+	//	back(20);
+	//}	
 }
